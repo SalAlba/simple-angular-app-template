6b27086955b3497ca2c10ee2b2919644dbf7046b
Sal <saljan93@live.com>
2019-11-25 19:10:26 +0100

    init app

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// modules ...
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from "./core/core.module";
+import { HomeModule } from "./modules/home/home.module";
+import { PostesModule } from "./modules/postes/postes.module";
+
+// components ...
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    HomeModule, // in anular page ex. no import !!!  https://stackblitz.com/angular/aqqyljyojye
+    PostesModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+
+@NgModule({
+  declarations: [FooterComponent, HeaderComponent, PageNotFoundComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ]
+})
+export class CoreModule { }
