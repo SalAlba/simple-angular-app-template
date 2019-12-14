9e83800a21e1d914515106bbb90b9faac6dd5124
Sal <saljan93@live.com>
2019-12-14 14:06:07 +0100

    add headr and footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+// modules ...
+import { MaterialModule } from "../shared/modules/material.module";
 
 
 @NgModule({
   declarations: [FooterComponent, HeaderComponent, PageNotFoundComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    MaterialModule
   ],
   exports: [
     HeaderComponent,
